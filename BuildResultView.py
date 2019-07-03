bcf897696bc5a89f4f684399957cf3512e54dd28
Håkon Stormo <hstormo@fastmail.com>
2019-07-03 22:24:50 +0200

    Refactor, allow building in multiple windows at the same time

--- BuildResultView.py
+++ BuildResultView.py
@@ -2,99 +2,113 @@ import sublime, sublime_plugin
 
 class BuildResultView(sublime_plugin.EventListener):
 
+    class Context:
+        def __init__(self, window, out_view, last_read, buffer):
+            self.window = window
+            self.out_view = out_view
+            self.last_read = last_read
+            self.buffer = buffer
+
     def __init__(self):
-        self.in_view = None
-        self.out_view = None
-        self.last_read = 0
-        self.buffer = ""
+        self.context_table = {}
 
-    def on_query_context(self, view, key, operator, operand, match_all):
-        if (key != "BuildResultView"): return
+    def on_post_window_command(self, window, command_name, args):
+        if command_name != "build": return
+        if args != None and args.get('select', False): return
 
-        window = view.window()
+        # Get build result panel and create context if it doesn't exist
+        build_panel = window.find_output_panel("exec")
+        if build_panel.id() not in self.context_table:
+            self.context_table[build_panel.id()] = self.Context(window, None, 0, "")
 
-        self.last_read = 0;
+        context = self.context_table[build_panel.id()]
 
-        self.orig_group = window.active_group()
-        self.orig_view = window.active_view()
+        context.last_read = 0
 
-        if self.out_view == None or -1 in window.get_view_index(self.out_view):
-            def create_out_view():
-                self.out_view = window.new_file()
-                self.out_view.set_name("Build result")
-                self.out_view.set_scratch(True)
-                self.out_view.set_read_only(True)
-                self.try_copy_settings()
+        # We save these values now, so that we don't lose them when we create a new output view.
+        orig_view = window.active_view()
+        orig_group = window.active_group()
 
-                if len(self.buffer) > 0:
-                    self.out_view.run_command("overwrite_view", {'content': self.buffer})
-                    self.buffer = ""
+        if context.out_view == None or -1 in context.window.get_view_index(context.out_view):
+            context.out_view = None
 
-                self.move_out_view()
+            # Try to find an output view from an earlier session
+            for view in context.window.views():
+                if view.settings().get("is_build_result_output_view", False):
+                    context.out_view = view
+                    break
 
-            sublime.set_timeout(create_out_view, 10)
+            if context.out_view == None:
+                # Create new output view
+                new_view = window.new_file()
+                new_view.set_name("Build results")
+                new_view.set_scratch(True)
+                new_view.set_read_only(True)
+                new_view.settings().set("is_build_result_output_view", True)
+                context.out_view = new_view
+
+        # Copy settings from build build_panel view to output view
+        build_panel_settings = build_panel.settings()
+        for key in ["result_file_regex",
+                    "result_line_regex",
+                    "result_base_dir",
+                    "word_wrap",
+                    "line_numbers",
+                    "gutter",
+                    "scroll_past_end"]:
+            context.out_view.settings().set(key, build_panel_settings.get(key))
+        context.out_view.assign_syntax(build_panel_settings.get("syntax"))
+
+        # Move output view if necessary
+        if orig_view.id() != context.out_view.id():
+            window.focus_view(context.out_view)
+            if window.num_groups() > 1:
+                target_group = orig_group + 1
+                if target_group >= window.num_groups():
+                    target_group = orig_group - 1
+                window.set_view_index(context.out_view, target_group, 0)
+                window.focus_view(orig_view)
+
+        # Clear output view, and fill it with any buffered contents
+        context.out_view.run_command("write_to_output_view", {
+            'content': context.buffer,
+            'begin': 0,
+            'end': context.out_view.size()
+        })
+        context.buffer = ""
+
+    def on_modified(self, modified_view):
+        # Check if the modified view is a known build panel
+        context = None
+        for panel_id in self.context_table:
+            if panel_id == modified_view.id():
+                context = self.context_table[panel_id]
+                break
+        if context == None: return
+
+        begin = context.last_read
+        end = modified_view.size()
+        content = modified_view.substr(sublime.Region(begin, end))
+
+        # If we don't have an output view for some reason, buffer the output until we do
+        if context.out_view == None:
+            context.buffer += content
         else:
-            self.out_view.run_command("overwrite_view", {'content': ""})
-            sublime.set_timeout(self.move_out_view, 10)
+            # If we have buffered content, prepend it
+            if len(context.buffer) > 0:
+                content = context.buffer + content
+                context.buffer = ""
+            context.out_view.run_command("write_to_output_view", {
+                'content': content,
+                'begin': begin,
+                'end': end
+            })
 
-        def get_in_view():
-            self.in_view = window.find_output_panel("exec")
-            self.try_copy_settings()
-        sublime.set_timeout(get_in_view, 1)
+        context.last_read = end
 
-    def try_copy_settings(self):
-        if self.out_view != None and self.in_view != None:
-            in_settings = self.in_view.settings()
-            for key in ["result_file_regex",
-                        "result_line_regex",
-                        "result_base_dir",
-                        "word_wrap",
-                        "line_numbers",
-                        "gutter",
-                        "scroll_past_end"]:
-                self.out_view.settings().set(key, in_settings.get(key))
-            self.out_view.assign_syntax(in_settings.get("syntax"))
-
-    def move_out_view(self):
-        if self.orig_view.id() == self.out_view.id(): return
-
-        window = self.out_view.window()
-        window.focus_view(self.out_view)
-
-        if window.num_groups() > 1:
-            target_group = self.orig_group + 1
-            if target_group >= window.num_groups():
-                target_group = self.orig_group - 1
-            window.set_view_index(self.out_view, target_group, 0)
-            window.focus_view(self.orig_view)
-
-    def on_modified(self, view):
-        if self.in_view == None or view.id() != self.in_view.id(): return
-
-        begin = self.last_read
-        end = view.size()
-        content = view.substr(sublime.Region(begin, end))
-
-        if self.out_view == None:
-            self.buffer += content
-        else:
-            if len(self.buffer) > 0:
-                content = self.buffer + content
-                self.buffer = ""
-            self.out_view.run_command("write_view", {'content': content, 'begin': begin, 'end': end})
-
-        self.last_read = end
-
-class WriteView(sublime_plugin.TextCommand):
+class WriteToOutputView(sublime_plugin.TextCommand):
     def run(self, edit, content, begin, end):
         self.view.set_read_only(False)
         self.view.replace(edit, sublime.Region(begin, end), content)
         self.view.set_read_only(True)
         self.view.show(end)
-
-class OverwriteView(sublime_plugin.TextCommand):
-    def run(self, edit, content):
-        self.view.set_read_only(False)
-        self.view.replace(edit, sublime.Region(0, self.view.size()), content)
-        self.view.set_read_only(True)
-        self.view.show(self.view.size())
